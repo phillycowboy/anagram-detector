1f88741d27995b7bc2c644301f0f84e0370c1b7e
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-11-16 16:41:19 -0500

    commit tree 5bfa31bda99eb81b394ab1ac73dca100cc3f9503

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require 'pry'
+class Anagram 
+    attr_accessor :word
+    def initialize(word)
+        @word = word 
+    end
+
+    def match(array)
+        new_arr = []
+        array.each do |word|
+            # binding.pry
+            if @word.split("").sort == word.split("").sort
+                new_arr<<word 
+            end
+        end
+        new_arr
+    end
+
+
+end
